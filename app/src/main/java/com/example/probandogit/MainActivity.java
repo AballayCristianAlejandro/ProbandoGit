7c0a1175f9b48f215412f84d8e3e01976ebabef9
AballayCristianAlejandro <64992973+AballayCristianAlejandro@users.noreply.github.com>
2021-03-17 08:32:42 -0300

    Se creo en el main los metodos onStart y onStop

--- app/src/main/java/com/example/probandogit/MainActivity.java
+++ app/src/main/java/com/example/probandogit/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.probandogit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("mensaje ","estoy en el onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 }
